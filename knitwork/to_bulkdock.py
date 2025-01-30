f827316a622b1149c3e366dfe51ed45c9b994a8b
Max Winokan <mwinokan@me.com>
2025-01-30 17:23:55 +0000

    lint

--- knitwork/to_bulkdock.py
+++ knitwork/to_bulkdock.py
@@ -9,57 +9,60 @@ import json
 
 app = Typer()
 
+
 @app.command()
 def create_bulkdock_inputs(subdir: str):
 
-	subdir = Path(subdir)
-	assert subdir.exists()
+    subdir = Path(subdir)
+    assert subdir.exists()
 
-	key = subdir.name
+    key = subdir.name
 
-	dir_suffix_pure_triples = [
-		(subdir/'knitwork_pure_output', '_pure_merge.json', True),
-		(subdir/'knitwork_impure_output', '_prop_pharmfp_impure_merge.json', False),
-	]
+    dir_suffix_pure_triples = [
+        (subdir / "knitwork_pure_output", "_pure_merge.json", True),
+        (subdir / "knitwork_impure_output", "_prop_pharmfp_impure_merge.json", False),
+    ]
 
-	for out_dir, suffix, pure in dir_suffix_pure_triples:
+    for out_dir, suffix, pure in dir_suffix_pure_triples:
 
-		all_merges = []
+        all_merges = []
 
-		for json_file in Path(out_dir).glob(f'*{suffix}'):
+        for json_file in Path(out_dir).glob(f"*{suffix}"):
 
-			merge_name = json_file.name.removesuffix(suffix)
+            merge_name = json_file.name.removesuffix(suffix)
 
-			hit_names = merge_name.split("-")
+            hit_names = merge_name.split("-")
 
-			data = json.load(open(json_file,'rt'))
+            data = json.load(open(json_file, "rt"))
 
-			for series in data.values():
+            for series in data.values():
 
-				logger.var(f'{merge_name} #expansions:', len(series['expansions']))
+                logger.var(f"{merge_name} #expansions:", len(series["expansions"]))
 
-				for i, (smiles, cmpd_ids) in enumerate(zip(series['expansions'], series['cmpd_ids'])):
+                for i, (smiles, cmpd_ids) in enumerate(
+                    zip(series["expansions"], series["cmpd_ids"])
+                ):
 
-					merge = dict(smiles=smiles)
+                    merge = dict(smiles=smiles)
 
-					for j,hit in enumerate(hit_names):
-						merge[j] = hit
+                    for j, hit in enumerate(hit_names):
+                        merge[j] = hit
 
-					all_merges.append(merge)
+                    all_merges.append(merge)
 
-		df = pd.DataFrame(all_merges)
+        df = pd.DataFrame(all_merges)
 
-		logger.success(f'{len(df)} total merges')
+        logger.success(f"{len(df)} total merges")
 
-		if pure:
-			logger.writing(f'{key}_knitwork_pure.csv')
-			df.to_csv(f"{key}_knitwork_pure.csv", index=False)
-		else:
-			logger.writing(f'{key}_knitwork_impure.csv')
-			df.to_csv(f"{key}_knitwork_impure.csv", index=False)
+        if pure:
+            logger.writing(f"{key}_knitwork_pure.csv")
+            df.to_csv(f"{key}_knitwork_pure.csv", index=False)
+        else:
+            logger.writing(f"{key}_knitwork_impure.csv")
+            df.to_csv(f"{key}_knitwork_impure.csv", index=False)
 
 
-if __name__ == '__main__':
-	app()
+if __name__ == "__main__":
+    app()
 
 # python to_bulkdock.py iter1_frags
